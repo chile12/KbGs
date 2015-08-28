a061d65079d60f3c4f60aed1c34a6c89d9fc16be
chile12 <markus.freudenberg@gmail.com>
2015-08-28 14:49:20 +0200

    waiting for sorted file

--- src/main/java/org/aksw/kbgs/processors/PropertyCompProcessor.scala
+++ src/main/java/org/aksw/kbgs/processors/PropertyCompProcessor.scala
@@ -1,5 +1,7 @@
 package org.aksw.kbgs.processors
 
+import java.io.File
+
 import akka.actor.{Actor, ActorRef}
 import org.aksw.kbgs.Main._
 import org.aksw.kbgs.{InitProcessStruct, Main}
@@ -14,11 +16,16 @@ class PropertyCompProcessor(boss: ActorRef, evalWriter: ActorRef)  extends Actor
   private var inputEmpty = false
   override def startProcess(): Unit =
   {
-    val sortedFileName = Main.config.outFile.substring(0, Main.config.outFile.indexOf(".nq.gz")) + "Sorted.nq.gz"
+    val sortedFileName =  new File(Main.config.outFile.substring(0, Main.config.outFile.indexOf(".nq.gz")) + "Sorted.nq.gz")
     sys.process.stringSeqToProcess(Seq("bash", "-c", "sort --parallel=8 -uo " + sortedFileName + " " + Main.config.outFile))
+    while(!sortedFileName.exists())
+    {
+      Thread.sleep(100)
+    }
+    System.out.println("sorting done into " + sortedFileName)
     val inits = new InitProcessStruct[StringBuilder]()
     inits.broadcastId = "compareKb"
-    inits.sourceFile = sortedFileName
+    inits.sourceFile = sortedFileName.getAbsolutePath
     inits.workerCount = 4
     val zz = classOf[KbComparatWorker]
     inits.classTag = ClassTag(zz)
